2434d1699ffa6b00d0fd3ca064d574a84604263d
Awren Nuit <54560441+AwrenNuit@users.noreply.github.com>
2020-01-07 21:49:06 -0600

    cleaned up code

--- client.js
+++ client.js
@@ -1,8 +1,6 @@
-$(document).ready(beginJQ);
-function beginJQ(){
-  console.log('Hello');
+$(function(){
   $('#startApp').on('click', onClick);
-}
+});
 
 const employees = [
   {
@@ -37,33 +35,7 @@ const employees = [
   }
 ];
 
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
-
-// Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
-// This problem is massive! Break the problem down. Use the debugger.
-// What is the fewest lines of code I can write and test to get just a little closer?
-
-// This is not a race. Everyone on your team should understand what is happening.
-// Ask questions when you don't.
-
-console.log( employees );
-console.log('');
-
-function employeeUpdate(employee){
-  let employeeName = {
-    name: employee.name,
-    employeeNumber: employee.employeeNumber,
-    annualSalary: employee.annualSalary,
-    reviewRating: employee.reviewRating
-  }
-  console.log('employeeUpdate: ', employeeName);
-}
-
-for(let i=0; i<employees.length; i++){
-employeeUpdate(employees[i]);
-}
-
-console.log('');
+let count = 0;
 
 function bonusCalc(employee){
   let bonus = {
@@ -73,43 +45,39 @@ function bonusCalc(employee){
     totalBonus: 0,
     employeeNumber: employee.employeeNumber
   }
-  if(employee.reviewRating === 3){
-    bonus.bonusPercentage += 4;
-  }
-  else if(employee.reviewRating === 4){
-    bonus.bonusPercentage += 6;
-  }
-  else if(employee.reviewRating === 5){
-    bonus.bonusPercentage += 10;
-  }
-  if(employee.employeeNumber.length === 4){
-    bonus.bonusPercentage += 5;
-  }
-  if(employee.annualSalary > 65000){
-    bonus.bonusPercentage --;
-  }
-  if(bonus.bonusPercentage > 13){
-    bonus.bonusPercentage = 13;
-  }
-  if(bonus.bonusPercentage < 0){
-    bonus.bonusPercentage = 0;
-  }
-  bonus.totalCompensation = (employee.annualSalary * (1 + bonus.bonusPercentage * 0.01));
-  bonus.totalBonus = (employee.annualSalary * (bonus.bonusPercentage * 0.01));
-  console.log('bonusCalc: ', bonus);
+  let rating = employee.reviewRating;
+  let number = employee.employeeNumber.length;
+  let salary = employee.annualSalary;
+  calculateBonus(bonus, rating, number, salary);
+  appendToDOM(bonus);
+}
+
+function calculateBonus(bonus, rating, number, salary){
+  rating === 3 ? bonus.bonusPercentage += 4 : '';
+  rating === 4 ? bonus.bonusPercentage += 6 : '';
+  rating === 5 ? bonus.bonusPercentage += 10 : '';
+  number === 4 ? bonus.bonusPercentage += 5 : '';
+  salary > 65000 ? bonus.bonusPercentage-- : '';
+  bonus.bonusPercentage > 13 ? bonus.bonusPercentage = 13 : '';
+  bonus.bonusPercentage < 0 ? bonus.bonusPercentage = 0 : '';
+  bonus.totalCompensation = (salary * (1 + bonus.bonusPercentage * 0.01));
+  bonus.totalBonus = (salary * (bonus.bonusPercentage * 0.01));
+}
+
+function appendToDOM(bonus){
   $('#output').append(`<div id="employeeContainer">`);
   $('#output').append(`<p> Employee Name: ${bonus.name} </p>`);
   $('#output').append(`<p> Bonus Percentage: ${bonus.bonusPercentage}% </p>`);
-  $('#output').append(`<p> Total Compensation: ${bonus.totalCompensation} </p>`);
+  $('#output').append(`<p> Total Compensation: ${bonus.totalCompensation.toFixed(0)} </p>`);
   $('#output').append(`<p> Total Bonus: ${bonus.totalBonus} </p>`);
   $('#output').append(`</div>`);
 }
-function onClick(){
-  for(let i=0; i<employees.length; i++){
-    bonusCalc(employees[i]);
-  }
-}
 
-for(let i=0; i<employees.length; i++){
-  bonusCalc(employees[i]);
+function onClick(){
+  if(count === 0){
+    for(let employee of employees){
+      bonusCalc(employee);
+    }
+    count++;
+  }
 }
